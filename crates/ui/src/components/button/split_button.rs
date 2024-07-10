eb71ffba96a96b8e32c3a34c821d2ea607f612d7
Nate Butler <iamnbutler@gmail.com>
2024-07-10 12:04:44 -0400

    wip

--- crates/ui/src/components/button/split_button.rs
+++ crates/ui/src/components/button/split_button.rs
@@ -1,13 +1,13 @@
-use gpui::{AnyElement, AnyView, ClickEvent, CursorStyle, ElementId, IntoElement, WindowContext};
+use gpui::{AnyElement, AnyView, ClickEvent, CursorStyle, ElementId, IntoElement, View, WindowContext};
 use crate::{
-    prelude::*, ButtonCommon, ButtonLike, ButtonLikeRounding, ButtonSize, ButtonStyle, ElevationIndex, IconButton, IconName
+    prelude::*, ButtonCommon, ButtonLike, ButtonLikeRounding, ButtonSize, ButtonStyle, ContextMenu, ElevationIndex, IconButton, IconName, PopoverMenu
 };
 use smallvec::SmallVec;
 
 #[derive(IntoElement)]
 pub struct SplitButton {
-    main_button: ButtonLike,
-    dropdown_button: ButtonLike,
+    button: ButtonLike,
+    dropdown_menu: AnyElement,
     children: SmallVec<[AnyElement; 2]>,
 }
 
@@ -18,79 +18,72 @@ impl SplitButton {
 
         let element_id = |suffix| ElementId::Name(format!("{}-{}", id, suffix).into());
 
-        Self {
-            main_button: ButtonLike::new(element_id("main"))
-                .child(div().px_0p5().child(Icon::new(icon)))
-                .rounding(ButtonLikeRounding::Left),
-            dropdown_button: ButtonLike::new(element_id("dropdown"))
+        let dropdown_menu = PopoverMenu::new(element_id("menu"))
+            .menu(move |cx| {
+                ContextMenu::build(cx, move |menu, _cx| {
+                    menu.header("REPL")
+                }).into()
+            }).trigger(ButtonLike::new(element_id("dropdown"))
                 .child(Icon::new(IconName::ChevronDownSmall).size(IconSize::XSmall))
                 .width(rems(1.).into())
-                .rounding(ButtonLikeRounding::Right),
+                .rounding(ButtonLikeRounding::Right));
+
+
+        Self {
+            button: ButtonLike::new(element_id("main"))
+                .child(div().px_0p5().child(Icon::new(icon)))
+                .rounding(ButtonLikeRounding::Left),
+            dropdown_menu: dropdown_menu.into_any_element(),
             children: SmallVec::new(),
         }
-    }
-
-    /// Sets the on_click handler for the dropdown button.
-    pub fn on_dropdown_click(
-        mut self,
-        handler: impl Fn(&ClickEvent, &mut WindowContext) + 'static,
-    ) -> Self {
-        self.dropdown_button = self.dropdown_button.on_click(handler);
-        self
-    }
-
-    /// Set the tooltip for the dropdown button.
-    pub fn dropdown_tooltip(mut self, tooltip: impl Fn(&mut WindowContext) -> AnyView + 'static) -> Self {
-        self.dropdown_button = self.dropdown_button.tooltip(tooltip);
-        self
     }
 }
 
 impl ButtonCommon for SplitButton {
     fn id(&self) -> &ElementId {
-        self.main_button.id()
+        self.button.id()
     }
 
     fn style(mut self, style: ButtonStyle) -> Self {
-        self.main_button = self.main_button.style(style);
-        self.dropdown_button = self.dropdown_button.style(style);
+        self.button = self.button.style(style);
+        self.popover_button = self.popover_button.style(style);
         self
     }
 
     fn size(mut self, size: ButtonSize) -> Self {
-        self.main_button = self.main_button.size(size);
-        self.dropdown_button = self.dropdown_button.size(size);
+        self.button = self.button.size(size);
+        self.popover_button = self.popover_button.size(size);
         self
     }
 
     fn tooltip(mut self, tooltip: impl Fn(&mut WindowContext) -> AnyView + 'static) -> Self {
-        self.main_button = self.main_button.tooltip(tooltip);
+        self.button = self.button.tooltip(tooltip);
         self
     }
 
     fn layer(mut self, elevation: ElevationIndex) -> Self {
-        self.main_button = self.main_button.layer(elevation);
-        self.dropdown_button = self.dropdown_button.layer(elevation);
+        self.button = self.button.layer(elevation);
+        self.popover_button = self.popover_button.layer(elevation);
         self
     }
 }
 
 impl Clickable for SplitButton {
     fn on_click(mut self, handler: impl Fn(&ClickEvent, &mut WindowContext) + 'static) -> Self {
-        self.main_button = self.main_button.on_click(handler);
+        self.button = self.button.on_click(handler);
         self
     }
 
     fn cursor_style(mut self, cursor_style: CursorStyle) -> Self {
-        self.main_button = self.main_button.cursor_style(cursor_style);
+        self.button = self.button.cursor_style(cursor_style);
         self
     }
 }
 
 impl Disableable for SplitButton {
     fn disabled(mut self, disabled: bool) -> Self {
-        self.main_button = self.main_button.disabled(disabled);
-        self.dropdown_button = self.dropdown_button.disabled(disabled);
+        self.button = self.button.disabled(disabled);
+        self.popover_button = self.popover_button.disabled(disabled);
         self
     }
 }
@@ -104,8 +97,8 @@ impl ParentElement for SplitButton {
 impl RenderOnce for SplitButton {
     fn render(self, _cx: &mut WindowContext) -> impl IntoElement {
         h_flex()
-            .child(self.main_button)
-            .child(self.dropdown_button)
+            .child(self.button)
+            .child(self.popover_button)
             // todo: add dropdown menu
     }
 }
